031c3c072e79c195fa19cdceb2d34ee96e2f5a27
novitasalsabila <novnovv10@gmail.com>
2023-03-23 16:13:45 +0700

    Membuat inputan dengan menggunakan perulangan while

--- InsertionSort_136/InsertionSort_136.cpp
+++ InsertionSort_136/InsertionSort_136.cpp
@@ -6,7 +6,22 @@ int n;  // Membuat variabel inputan n
 
 
 void input() {
+    while (true)
+    {
+        cout << "Masukan jumlah data pada array : "; //Membuat inputan jumlah element array
+        cin >> n;                                    //Memanggil variabel inputan
 
+        if (n <= 20)
+        {
+            // Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+
+        else
+
+            cout << "\narray yang anda masukan maksimal 20 elemen. \n"; // Menampilkan pesan jika data lebih dari 20
+
+    }
 }
 
 int main()
