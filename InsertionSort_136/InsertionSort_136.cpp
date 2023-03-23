4c10959a93e1e1403deb00ab5129f2418f25178f
novitasalsabila <novnovv10@gmail.com>
2023-03-23 16:23:38 +0700

    looping output

--- InsertionSort_136/InsertionSort_136.cpp
+++ InsertionSort_136/InsertionSort_136.cpp
@@ -73,6 +73,12 @@ void display() {
     cout << "\n========================" << endl; // Output kelayar
     cout << "Element Array yang telah tersusun " << endl;  // Output kelayar
     cout << "\n========================" << endl;
+
+    for (int j = 0;j < n; j++)
+    {                           // Looping dengan j mulai dari 0 hingga n-1
+        cout << arr[j] << endl; // Output kelayar
+    }
+    cout << endl;   // Output baris kosong
 }
 
 int main()
